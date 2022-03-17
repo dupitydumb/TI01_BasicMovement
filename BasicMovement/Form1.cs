63d324ff94ab222b0a503a857f62394d0ccb4b62
AvKov <AvKov@AVKOV>
2022-03-17 14:52:27 +0700

    Add project files.

---
+++ BasicMovement/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicMovement
+{
+    public partial class Form1 : Form
+    {
+        //Deklarasi variabel X dan Y
+        private int _x;
+        private int _y;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            // Initial location
+            _x = 50;
+            _y = 50;
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // Object Ractangle Di posisi _x _y dengan ukuran 200
+            e.Graphics.FillRectangle(Brushes.DarkMagenta, _x, _y, 100, 100);
+
+
+        }
+    }
+}
