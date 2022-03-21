aa8f275bd0ba916ab8dcc6ad861d9ff2bfbb1c7a
AvKov <inknowgt@gmail.com>
2022-03-21 21:07:25 +0700

    Added Files

--- BasicMovement/Form1.Designer.cs
+++ BasicMovement/Form1.Designer.cs
@@ -46,27 +46,24 @@ namespace BasicMovement
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(100, 180);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(300, 180);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(0, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(200, 70);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(0, 13);
             this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
             // 
             // Form1
             // 
